e9e48995250ece7c15d40012f566693e8d0923ff
Brandon Reid <brandonreid2@my.unt.edu>
2017-04-08 15:00:43 -0500

    Create Lab2A.cpp

---
+++ Lab2/Lab2A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+	 cout << "This program contains several syntax errors.\n";
+	 cout << "These need to be fixed before you submit it.\n";
+	 cout << "Fixed by [Bar0191]\n";
+	 return 0;
+
+}
