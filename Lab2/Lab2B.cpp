da616809001b8beaa0a22e8d287166b404d2b770
Brandon Reid <brandonreid2@my.unt.edu>
2017-04-08 15:00:53 -0500

    Add files via upload

---
+++ Lab2/Lab2B.cpp
@@ -0,0 +1,17 @@
+// 1. Declare variable for integer
+// 2. Prompt for integer
+// 3. read statement and integer
+
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+int grade;
+
+	cout << "Enter the grade average you expect as an integer:\n";
+	cin >> grade;
+	cout << "I, Brandon Reid, expect to get a "<<grade<<" in this course.\n";
+	return 0;
+
+}
